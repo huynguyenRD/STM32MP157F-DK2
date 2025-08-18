5f981b5897b65dfdd54c20c9e9d2cc7059d46e45
Huy Nguyen <realhuy35@gmail.com>
2025-08-18 10:55:59 +0700

    Add sysmon package for STM32MP157F-DK2
    - Add buildroot package configuration for sysmon
    - Include source files: sysmon.c (main application) and fb_draw.c (framebuffer drawing utilities)
    - Makefile configured for cross-compilation with buildroot
    - Package successfully builds ARM binary for target system

---
+++ sysmon/src/fb_draw.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int fb_draw_boxes(int cpu_pct, int mem_pct, int temp_c);
---
+++ sysmon/src/sysmon.c
@@ -0,0 +1,7 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+
+int main(void) {
+    printf("Sysmon application started\n");
+    return 0;
+}
